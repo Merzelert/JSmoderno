8b4b661668557a61f56f0d7e74d496b0ec75643b
Erik Ivan Retana Lopez <merzelert@gmail.com>
2022-07-28 18:27:38 -0500

    variables

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,24 @@
+//Inicializar una variable con un valor
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+//Las variables se pueden reasignar 
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+//JavaScript es un lenguaje tipo dinamico, no se especifica tipo de dato
+producto = 20;
+console.log(producto);
+
+//Se pueden inicializar sin valor y asignarlo despues
+var disponible;
+disponible = true;
+disponible = false;
+
+//Inicializar multiples variables
+var categoria = "Computadoras",
+    marca = "Apple",
+    calificacion = 5;
+
+//Las variables no se pueden iniciar con numeros 
+
+//Estilo camel 
+var nombreProducto;
