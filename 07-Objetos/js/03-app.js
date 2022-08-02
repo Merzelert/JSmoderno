faa4afda7055935fda9a87d31d1a929d43a27086
Erik Ivan Retana Lopez <merzelert@gmail.com>
2022-08-02 09:37:25 -0500

    objetos

---
+++ 07-Objetos/js/03-app.js
@@ -0,0 +1,15 @@
+const producto = {
+    nombre : "Monitor 20 pulgadas",
+    precio : 300,
+    disponible: true
+}
+
+//Destructuring 
+
+const { nombre, precio, disponible } = producto;
+
+console.log(nombre);
+console.log(precio);
+console.log(disponible);
+
+
