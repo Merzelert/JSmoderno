625e0a73570992ca451eeb091110ef3fb4ee4f5d
Erik Ivan Retana Lopez <merzelert@gmail.com>
2022-08-09 09:36:32 -0500

    Array Methods filter

---
+++ 12-ArrayMethods/js/04-app.js
@@ -0,0 +1,18 @@
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+]
+
+// metodo .filter
+
+let resultado;
+
+resultado = carrito.filter ( producto => producto.precio > 300)
+resultado = carrito.filter ( producto => producto.precio < 300)
+
+console.log(resultado)
+
